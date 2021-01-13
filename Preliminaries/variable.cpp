c9ff6d84ceae2955e5737785f7144bf4b827937b
fidele000 <fihacker000@gmail.com>
2021-01-13 09:56:06 +0300

    updates

--- Preliminaries/variable.cpp
+++ Preliminaries/variable.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 int main (void){
-    int workDays;
+    int workDays=5;
     float workHours, payRate, weeklyPay;
-    workDays = 5;
-    workHours = 7.5;
-    
-    std::cout << "What is the hourly pay rate? ";
-    std::cin >> payRate;
-
-    weeklyPay = workDays  * workHours * payRate;
-    std::cout << "Weekly Pay = ";
-    std::cout << weeklyPay;
+    std::cout << "What are the work hours and the hourly pay rate? ";
+    std::cin >> workHours >> payRate;
+    weeklyPay = workDays * workHours * payRate;
+    std::cout << "Weekly Pay = " << weeklyPay << '\n';
 }
