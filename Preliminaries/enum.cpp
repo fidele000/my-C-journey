c5625cb70e4d1f96944effabb6bf03ec6e35e9c6
fidele000 <fihacker000@gmail.com>
2021-01-16 17:55:09 +0300

    updates

--- Preliminaries/enum.cpp
+++ Preliminaries/enum.cpp
@@ -6,9 +6,18 @@ enum class Color{
     yellow
 };
 
+Color& operator++(Color& t){
+    switch (t)
+    {
+    case Color::red: return t=Color::green;
+    case Color::green: return t=Color::yellow;
+    case Color::yellow: return t=Color::red;
+    
+    } 
+}
 
 
 int main(){
     Color color=Color::red;
-    cout<<color;
+    Color next=++color;
 }
