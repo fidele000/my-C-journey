c49e17a663626574c5c753e851a75e8fbead8f6e
fidele000 <fihacker000@gmail.com>
2021-01-16 20:32:05 +0300

    updates

---
+++ Preliminaries/class2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Vector{
+    public:
+        Vector(int s);
+        double& operator[](int i);
+        int size();
+    private:
+        double* elem;
+        int sz;
+
+}
+
+
+Vector::Vector(int s) 
+    :elem({new double[s]}),sz{s}{}
+
+double& Vector::operator[](int i){
+    return elem[i];
+}
+
+int Vector::size(){
+    return sz;
+}
+
+
+int main(){
+    Vector v(5);
+
+    cout<<"Enter Inputs"<<endl;
+    for (int i = 0; i < v.size(); ++i)
+    {
+        cin>>v[i];
+    }
+
+    int sum=0;
+    for (int i = 0; i < v.size(); ++i)
+    {
+        sum+=v[i];
+    }
+    
+    cout<<"The Sum is "<<sum;
+    
+}
